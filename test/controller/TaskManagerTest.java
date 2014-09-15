04aca32c7f0bce32d51517dede601176366f4e78
Jireh <twajxjiraffe@gmail.com>
2014-09-15 22:07:47 +0800

    Updated test to clear .txt file after test completes.

--- test/controller/TaskManagerTest.java
+++ test/controller/TaskManagerTest.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,7 +17,8 @@ public class TaskManagerTest {
 
     @Test
     public void test() throws IOException {
-        testAddTrue("adding text", "hello");
+        testAddTrue("adding single word", "hello");
+        testAddTrue("adding multiple words", "hello there how are you doing today? the weather is lovely isn't it?");
         testAddFalse("adding null", null);
         testAddFalse("adding empty string", "");
     }
@@ -33,5 +35,10 @@ public class TaskManagerTest {
     public void setUp() throws IOException {
         cg = new CommandGenerator();
         tm = new TaskManager("text.txt");
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        tm.clear();
     }
 }
