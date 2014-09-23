8d83e17ba3eb2cc93348a8cdc3c66cdb3ef2c87f
Jiraffe7 <twajxjiraffe@gmail.com>
2014-09-23 16:09:58 +0800

    Added Command class skeleton.

--- src/logic/Command.java
+++ src/logic/Command.java
@@ -3,13 +3,59 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package logic;
 
 /**
  *
  * @author Jireh
  */
-public abstract class Command {
-    
+public class Command {
+
+    protected static boolean add() {
+
+    }
+
+    protected static boolean delete() {
+
+    }
+
+    protected static boolean clear() {
+
+    }
+
+    protected static boolean edit() {
+
+    }
+
+    protected static String display() {
+
+    }
+
+    protected static boolean sort() {
+
+    }
+
+    protected static String search() {
+
+    }
+
+    protected static boolean markDone() {
+
+    }
+
+    protected static boolean undo() {
+
+    }
+
+    protected static boolean redo() {
+
+    }
+
+    protected static boolean move() {
+
+    }
+
+    protected static void updateUndoAndRedoStacks() {
+
+    }
 }
