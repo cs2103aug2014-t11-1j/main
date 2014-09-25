cab133f2562069cfcdab82b682b38925c42db7ad
small son <xjysel@gmail.com>
2014-09-25 13:07:36 +0800

    Resource loader

--- src/gui/MainApp.java
+++ src/gui/MainApp.java
@@ -18,16 +18,19 @@ public class MainApp extends Application {
     private Stage primaryStage;
     private BorderPane rootLayout;
     private AnchorPane overallView;
+    private TrayApplication ta;
     
     private double xOffset = 0;
     private double yOffset = 0;
     
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Phantom");
         this.primaryStage.centerOnScreen();
         this.primaryStage.initStyle(StageStyle.TRANSPARENT);
+        ta = TrayApplication.getInstance();
+        ta.createTrayIcon(primaryStage);
 
         initRootLayout();
 
--- src/gui/TrayApplication.java
+++ src/gui/TrayApplication.java
@@ -1,5 +1,117 @@
 package gui;
 
-public class TrayApplication {
+import java.awt.AWTException;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import application.ResourceLoader;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+/**
+ * This class is used to initialize
+ * the tray application and to give
+ * the gui tray functionalities
+ * 
+ * * Author: smallson
+ */
+
+public class TrayApplication {
+	
+	private static TrayApplication ta = new TrayApplication();
+	private boolean firstTime = true;
+    private TrayIcon trayIcon;
+    private java.awt.Image image = null;
+	
+	private TrayApplication(){
+	}
+	
+	public static TrayApplication getInstance(){
+		return ta;
+	}
+	
+	public void createTrayIcon(final Stage stage) throws IOException {
+        if (SystemTray.isSupported()) {
+            SystemTray tray = SystemTray.getSystemTray();
+            image = ImageIO.read(ResourceLoader.load("ghost.png"));
+          
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent t) {
+                    hide(stage);
+                }
+            });
+            // create a action listener to listen for default action executed on the tray icon
+            final ActionListener closeListener = new ActionListener() {
+                @Override
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    System.exit(0);
+                }
+            };
+
+            ActionListener showListener = new ActionListener() {
+                @Override
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            stage.show();
+                        }
+                    });
+                }
+            };
+            // create a popup menu
+            PopupMenu popup = new PopupMenu();
+
+            MenuItem showItem = new MenuItem("Show");
+            showItem.addActionListener(showListener);
+            popup.add(showItem);
+
+            MenuItem closeItem = new MenuItem("Close");
+            closeItem.addActionListener(closeListener);
+            popup.add(closeItem);
+            // construct a TrayIcon
+            trayIcon = new TrayIcon(image, "Phantom", popup);
+            // set the TrayIcon properties
+            trayIcon.addActionListener(showListener);
+            // add the tray image
+            try {
+                tray.add(trayIcon);
+            } catch (AWTException e) {
+                System.err.println(e);
+            }
+        }
+    }
+
+    public void showProgramIsMinimizedMsg() {
+        if (firstTime) {
+            trayIcon.displayMessage("Phantom",
+                    " is running in the background",
+                    TrayIcon.MessageType.INFO);
+            firstTime = false;
+        }
+    }
+
+    private void hide(final Stage stage) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                if (SystemTray.isSupported()) {
+                	
+                    stage.hide();
+                    showProgramIsMinimizedMsg();
+                } else {
+                    System.exit(0);
+                }
+            }
+        });
+    }
 }
