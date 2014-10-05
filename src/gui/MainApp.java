5ed5f571547b26311c9dd611c1a35489d3f4900a
Gan Cheng Yee Krystal <colourkrystal@gmail.com>
2014-10-05 22:54:26 +0800

    Playing around with different menu bar placing.

--- src/gui/MainApp.java
+++ src/gui/MainApp.java
@@ -16,77 +16,96 @@ import javafx.stage.StageStyle;
 
 public class MainApp extends Application {
 
-    private Stage primaryStage;
-    private AnchorPane overallView;
-    private BorderPane rootLayout;
-    
-    private double xOffset = 0;
-    private double yOffset = 0;
-    
-    @Override
-    public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("Phantom");
-        this.primaryStage.centerOnScreen();
-        this.primaryStage.initStyle(StageStyle.TRANSPARENT);
-        
-        initRootLayout();
-        showPhantomOverallView();
-    }
-    
-    public void initRootLayout() {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
-            rootLayout = (BorderPane) loader.load();
+	private Stage primaryStage;
+	private AnchorPane overallView;
+	//private BorderPane rootLayout;
 
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-            
-            rootLayout.setOnMousePressed(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    xOffset = event.getSceneX();
-                    yOffset = event.getSceneY();
-                }
-            });
-            rootLayout.setOnMouseDragged(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    primaryStage.setX(event.getScreenX() - xOffset);
-                    primaryStage.setY(event.getScreenY() - yOffset);
-                }
-            });
-            
-            this.primaryStage.getIcons().add(new Image("file:Resources/images/ghost.png"));
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public void showPhantomOverallView() {
-        try {  	
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/OverallView.fxml"));
-            overallView = (AnchorPane) loader.load();
-            System.out.println("overall view loaded");
+	private double xOffset = 0;
+	private double yOffset = 0;
 
-            rootLayout.setCenter(overallView);
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Phantom");
+		this.primaryStage.centerOnScreen();
+		this.primaryStage.initStyle(StageStyle.TRANSPARENT);
 
-            PhantomController controller = loader.getController();
-            controller.setPrimaryStage(primaryStage);
-            controller.setOverallView(overallView);
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		//        initRootLayout();
+		showPhantomOverallView();
+	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	//    public void initRootLayout() {
+	//        try {
+	//            FXMLLoader loader = new FXMLLoader();
+	//            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+	//            rootLayout = (BorderPane) loader.load();
+	//
+	//            Scene scene = new Scene(rootLayout);
+	//            primaryStage.setScene(scene);
+	//            primaryStage.show();
+	//            
+	//            rootLayout.setOnMousePressed(new EventHandler<MouseEvent>() {
+	//                @Override
+	//                public void handle(MouseEvent event) {
+	//                    xOffset = event.getSceneX();
+	//                    yOffset = event.getSceneY();
+	//                }
+	//            });
+	//            rootLayout.setOnMouseDragged(new EventHandler<MouseEvent>() {
+	//                @Override
+	//                public void handle(MouseEvent event) {
+	//                    primaryStage.setX(event.getScreenX() - xOffset);
+	//                    primaryStage.setY(event.getScreenY() - yOffset);
+	//                }
+	//            });
+	//            
+	//            this.primaryStage.getIcons().add(new Image("file:Resources/images/ghost.png"));
+	//            
+	//        } catch (IOException e) {
+	//            e.printStackTrace();
+	//        }
+	//    }
+
+	public void showPhantomOverallView() {
+		try {  	
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/OverallView.fxml"));
+			overallView = (AnchorPane) loader.load();
+			System.out.println("overall view loaded");
+
+			Scene scene = new Scene(overallView);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+			//            rootLayout.setCenter(overallView);
+
+			PhantomController controller = loader.getController();
+			controller.setPrimaryStage(primaryStage);
+			controller.setOverallView(overallView);
+
+			overallView.setOnMousePressed(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					xOffset = event.getSceneX();
+					yOffset = event.getSceneY();
+				}
+			});
+			overallView.setOnMouseDragged(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					primaryStage.setX(event.getScreenX() - xOffset);
+					primaryStage.setY(event.getScreenY() - yOffset);
+				}
+			});
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
