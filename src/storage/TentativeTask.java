ca9848aa97f0d3fd839c5cb74f6495b15439d101
Gan Cheng Yee Krystal <colourkrystal@gmail.com>
2014-10-15 20:40:20 +0800

    Skeleton for GUI tentative view. Made changes to TentativeTask for building of tree.

--- src/storage/TentativeTask.java
+++ src/storage/TentativeTask.java
@@ -2,26 +2,57 @@ package storage;
 
 import java.util.ArrayList;
 import java.util.Date;
+
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.scene.control.TreeItem;
 
 /**
  *
- * @author Jireh
+ * @author Jireh and Krystal
  */
 public class TentativeTask {
-
-    private StringProperty eventStringProperty;
-
-    private String event;
+	
+	//used by Logic to check for clashes
     private static ArrayList<TimePeriod> globalBlockedTimePeriods;
     private ArrayList<TimePeriod> blockedTimePeriods;
-
-    public TentativeTask(String event) {
+    
+    //used by GUI to create tree
+    private String event;
+    private int position;
+	private ArrayList<String> dates;
+    
+    private TreeItem<TentativeNode> node;
+  
+    public TentativeTask(String event, int position) {
         blockedTimePeriods = new ArrayList<TimePeriod>();
-        this.event = event;
-        this.eventStringProperty = new SimpleStringProperty(event);
+		dates = new ArrayList<String>();
+		
+		//set dates?
+		
+        node = new TreeItem<TentativeNode>(new TentativeNode(event, String.valueOf(position)));
+        node.setExpanded(true);
+        
+        setEvent(event);
+        setPosition(position);
+        //setChildren();
     }
+	
+	//Mutators
+	public void setEvent(String event){
+		this.event = event;
+		node.getValue().setPosition(event);
+	}
+	
+	public void setPosition(int position) {
+		this.position = position;
+		node.getValue().setPosition(String.valueOf(position));
+	}
+
+	public void addDate(String date){
+		dates.add(date);
+		setChild(date);
+	}
 
     public boolean blockTimePeriod(TimePeriod tp) {
         for (TimePeriod period : globalBlockedTimePeriods) {
@@ -33,15 +64,29 @@ public class TentativeTask {
         blockedTimePeriods.add(tp);
         return true;
     }
-
-    public ArrayList<TimePeriod> getBlockedTimePeriods() {
-        return blockedTimePeriods;
-    }
-
+    
+    //Accessors
     public static ArrayList<TimePeriod> getGlobalBlockedTimePeriods() {
         return globalBlockedTimePeriods;
     }
 
+    public ArrayList<TimePeriod> getBlockedTimePeriods() {
+        return blockedTimePeriods;
+    }
+    
+	public String getEvent(){
+		return event;
+	}
+	
+	public ArrayList<String> getDates(){
+		return dates;
+	}
+	
+	public int getPosition(){
+		return position;
+	}
+	
+	//private methods
     private static boolean isValidTimePeriod(TimePeriod tp, TimePeriod toCompare) {
         Date start = tp.getStartDate();
         Date end = tp.getEndDate();
@@ -54,4 +99,18 @@ public class TentativeTask {
 
         return isStartBeforeEnd && isStartValid && isEndValid;
     }
+
+	private void setChildren(){
+		for(String date : dates){
+			TreeItem<TentativeNode> leaf = new TreeItem<TentativeNode>(new TentativeNode(date, null));
+			node.getChildren().add(leaf);
+			node.setExpanded(true);
+		}
+	}
+	
+	private void setChild(String date){
+		TreeItem<TentativeNode> leaf = new TreeItem<TentativeNode>(new TentativeNode(date, null));
+		node.getChildren().add(leaf);
+		node.setExpanded(true);
+	}
 }
