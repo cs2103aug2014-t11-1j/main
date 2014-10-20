45aae2d317c23bf6b0c182aa1f2d299893629073
Gan Cheng Yee Krystal <colourkrystal@gmail.com>
2014-10-20 15:45:41 +0800

    Updated TimePeriod class to include startTime and endTIme. 2 constructors available for user's preference.

--- src/storage/TimePeriod.java
+++ src/storage/TimePeriod.java
@@ -8,25 +8,57 @@ import java.util.Date;
  */
 public class TimePeriod {
 
-    private Date startDate, endDate;
-
-    public TimePeriod(Date startDate, Date endDate) {
+    private Date startDate, endDate, startTime, endTime;
+    
+    //constructors
+    public TimePeriod(){
+    	
+    }
+    
+    public TimePeriod(Date startDate, Date endDate, Date startTime, Date endTime) {
         this.startDate = startDate;
         this.endDate = endDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
-
-    /**
-     * @return the startDate
-     */
+    
+    //mutators
+    public void setStartDate(Date startDate){
+    	this.startDate = startDate;
+    }
+    
+    public void setEndDate(Date endDate){
+    	this.endDate = endDate;
+    }
+    
+    public void setStartTime(Date startTime){
+    	this.startTime = startTime;
+    }
+    
+    public void setEndTime(Date endTime){
+    	this.endTime = endTime;
+    }
+    
+    //accessors
     public Date getStartDate() {
         return startDate;
     }
 
-    /**
-     * @return the endDate
-     */
     public Date getEndDate() {
         return endDate;
+    }
+    
+    public Date getStartTime(){
+    	return startTime;
+    }
+    
+    public Date getEndTime(){
+    	return endTime;
+    }
+    
+    public String toString(){
+    	//TODO: make a toString function
+    	return null;
     }
 
 }
