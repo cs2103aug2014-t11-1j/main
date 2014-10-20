c1279caa9ec3a9782b919883cb432b73855f3efb
Jireh <twajxjiraffe@gmail.com>
2014-10-20 21:12:09 +0800

    Added toString to TimePeriod.

--- src/storage/TimePeriod.java
+++ src/storage/TimePeriod.java
@@ -1,5 +1,6 @@
 package storage;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -9,36 +10,46 @@ import java.util.Date;
 public class TimePeriod {
 
     private Date startDate, endDate, startTime, endTime;
-    
+    private SimpleDateFormat dateFormat, timeFormat;
+
     //constructors
-    public TimePeriod(){
-    	
+    public TimePeriod() {
+        startDate = new Date();
+        startTime = new Date();
+        endDate = new Date();
+        endTime = new Date();
+
+        dateFormat = new SimpleDateFormat("dd/mm/yyyy");
+        timeFormat = new SimpleDateFormat("hh:mm");
     }
-    
+
     public TimePeriod(Date startDate, Date endDate, Date startTime, Date endTime) {
         this.startDate = startDate;
         this.endDate = endDate;
         this.startTime = startTime;
         this.endTime = endTime;
+
+        dateFormat = new SimpleDateFormat("dd/mm/yyyy");
+        timeFormat = new SimpleDateFormat("hh:mm");
     }
-    
+
     //mutators
-    public void setStartDate(Date startDate){
-    	this.startDate = startDate;
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
     }
-    
-    public void setEndDate(Date endDate){
-    	this.endDate = endDate;
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
     }
-    
-    public void setStartTime(Date startTime){
-    	this.startTime = startTime;
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
     }
-    
-    public void setEndTime(Date endTime){
-    	this.endTime = endTime;
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
     }
-    
+
     //accessors
     public Date getStartDate() {
         return startDate;
@@ -47,18 +58,21 @@ public class TimePeriod {
     public Date getEndDate() {
         return endDate;
     }
-    
-    public Date getStartTime(){
-    	return startTime;
+
+    public Date getStartTime() {
+        return startTime;
     }
-    
-    public Date getEndTime(){
-    	return endTime;
+
+    public Date getEndTime() {
+        return endTime;
     }
-    
-    public String toString(){
-    	//TODO: make a toString function
-    	return null;
+
+    @Override
+    public String toString() {
+        String returnString = String.format("%s %s - %s %s", timeFormat.format(startTime),
+                dateFormat.format(startDate), timeFormat.format(endTime),
+                dateFormat.format(endDate));
+        return returnString;
     }
 
 }
---
+++ src/storage/TimePeriodTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package storage;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Jireh
+ */
+public class TimePeriodTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calendar cal = Calendar.getInstance();
+        TimePeriod test = new TimePeriod();
+
+        cal.set(2013, 1, 15);
+
+        test.setStartDate(cal.getTime());
+
+        System.out.println(test);
+    }
+
+}
