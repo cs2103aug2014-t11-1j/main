725dfaf3053628d13d65c1026210377d70b6f3c4
Gan Cheng Yee Krystal <colourkrystal@gmail.com>
2014-10-24 00:26:32 +0800

    TimePeriod class is now fully functional.

--- src/storage/TimePeriod.java
+++ src/storage/TimePeriod.java
@@ -1,95 +1,144 @@
 package storage;
 
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
- *
- * @author Jireh
+ * This class stores a time period set using String.
+ * Date string format is dd/mm/yyyy.
+ * Time string format is hhmm, 24 hour clock.
+ * 
+ * Start and end dates and times can be accessed through String
+ * or Date form. 
+ * 
+ * There must be a start date, start time and end time in every timePeriod.
+ * 
+ * Supports the toString method.
+ * 
+ * @author Krystal
  */
 public class TimePeriod {
 
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
-	private static final SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm");
-	
+	private static final SimpleDateFormat dateGetter = new SimpleDateFormat("dd/MM/yyyy");
+	private static final SimpleDateFormat timeGetter = new SimpleDateFormat("hhmm");
+	private static final SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("EEE, MMM d hh:mm");
+	private static final SimpleDateFormat timeOnlyFormatter = new SimpleDateFormat("hh:mm");
+
 	private String startDateString, endDateString, startTimeString, endTimeString;
-    private Date startDate, endDate;
-    
+	private Date startDate, endDate;
 
-    //constructors
-    public TimePeriod() {
-    }
 
-    public TimePeriod(String startDate, String endDate, String startTime, String endTime) {
-        this.startDateString = startDate;
-        this.endDateString = endDate;
-        this.startTimeString = startTime;
-        this.endTimeString = endTime;
-        
-        convertStartDate();
-        convertEndDate();
-    }
+	//constructors
+	public TimePeriod() {
+	}
 
-    //mutators
-    public void setStartDateString(String startDate) {
-        this.startDateString = startDate;
-        convertStartDate();
-    }
+	public TimePeriod(String startDate, String endDate, String startTime, String endTime) {
+		this.startDateString = startDate;
+		this.endDateString = endDate;
+		this.startTimeString = startTime;
+		this.endTimeString = endTime;
 
-    public void setEndDateString(String endDate) {
-        this.endDateString = endDate;
-        convertEndDate();
-    }
+		convertStartDate();
+		convertEndDate();
+	}
 
-    public void setStartTimeString(String startTime) {
-        this.startTimeString = startTime;
-        convertStartDate();
-    }
+	//mutators
+	public void setStartDateString(String startDate) {
+		this.startDateString = startDate;
+		convertStartDate();
+	}
 
-    public void setEndTimeString(String endTime) {
-        this.endTimeString = endTime;
-        convertEndDate();
-    }
+	public void setEndDateString(String endDate) {
+		this.endDateString = endDate;
+		convertEndDate();
+	}
 
-    //accessors
-    public String getStartDateString() {
-        return startDateString;
-    }
+	public void setStartTimeString(String startTime) {
+		this.startTimeString = startTime;
+		convertStartDate();
+	}
 
-    public String getEndDateString() {
-        return endDateString;
-    }
+	public void setEndTimeString(String endTime) {
+		this.endTimeString = endTime;
+		convertEndDate();
+	}
 
-    public String getStartTimeString() {
-        return startTimeString;
-    }
+	//accessors
+	public String getStartDateString() {
+		return startDateString;
+	}
 
-    public String getEndTimeString() {
-        return endTimeString;
-    }
-    
-    public Date getStartDate(){
-    	return startDate;
-    }
-    
-    public Date getEndDate(){
-    	return endDate;
-    }
+	public String getEndDateString() {
+		return endDateString;
+	}
 
-    @Override
-    public String toString() {
-        String returnString = String.format("%s %s - %s %s", timeFormat.format(startTimeString),
-                dateFormat.format(startDateString), timeFormat.format(endTimeString),
-                dateFormat.format(endDateString));
-        return returnString;
-    }
-    
-    private void convertStartDate(){
-    	
-    }
-    
-    private void convertEndDate(){
-    	
-    }
+	public String getStartTimeString() {
+		return startTimeString;
+	}
+
+	public String getEndTimeString() {
+		return endTimeString;
+	}
+
+	public Date getStartDate(){
+		return startDate;
+	}
+
+	public Date getEndDate(){
+		return endDate;
+	}
+
+	@Override
+	public String toString() {
+		String returnString;
+		
+		if(endDateString != null || !endDateString.equals("")){
+			returnString = dateTimeFormatter.format(startDate) + " - " + dateTimeFormatter.format(endDate);
+		}else{
+			returnString = dateTimeFormatter.format(startDate) + " - " + timeOnlyFormatter.format(endDate);
+		}
+		
+		return returnString;
+	}
+
+	private void convertStartDate(){
+		Calendar startDateCal = Calendar.getInstance();
+
+		if(startDateString != null || !startDateString.equals("")){
+			startDateCal.setTime(dateGetter.parse(startDateString, new ParsePosition(0)));
+		}
+
+		if(startTimeString != null || !startTimeString.equals("")){
+			Date temp = timeGetter.parse(startTimeString, new ParsePosition(0));
+			Calendar startTimeCal = Calendar.getInstance();
+			startTimeCal.setTime(temp);
+			startDateCal.set(Calendar.HOUR_OF_DAY, startTimeCal.get(Calendar.HOUR_OF_DAY));
+			startDateCal.set(Calendar.MINUTE, startTimeCal.get(Calendar.MINUTE));
+		}
+
+		startDate = startDateCal.getTime();
+	}
+
+	private void convertEndDate(){
+		Calendar endDateCal = Calendar.getInstance();
+
+		if(endDateString != null || !endDateString.equals("")){
+			endDateCal.setTime(dateGetter.parse(endDateString, new ParsePosition(0)));
+		}else if(startDateString != null || !startDateString.equals("")){
+			endDateCal.setTime(dateGetter.parse(startDateString, new ParsePosition(0)));
+		}
+
+		if(endTimeString != null || !endTimeString.equals("")){
+			Date temp = timeGetter.parse(endTimeString, new ParsePosition(0));
+			Calendar endTimeCal = Calendar.getInstance();
+			endTimeCal.setTime(temp);
+			endDateCal.set(Calendar.HOUR_OF_DAY, endTimeCal.get(Calendar.HOUR_OF_DAY));
+			endDateCal.set(Calendar.MINUTE, endTimeCal.get(Calendar.MINUTE));
+		}
+
+		endDate = endDateCal.getTime();
+	}
 
 }
