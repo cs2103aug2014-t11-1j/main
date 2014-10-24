e85652ce1921d178d9c5302ad2d3bb5e8d942c71
zhang yongkai <yongkai8@hotmail.com>
2014-10-25 00:05:02 +0800

    modified the timeservice class. and make changes to other files affected by this change. added testparser unit test again

--- src/gui/TimeService.java
+++ src/gui/TimeService.java
@@ -1,12 +1,11 @@
 package gui;
+
 /**
  * @author zhang
  * 
  * this time service class can be used for other timer services.
- * currently it has Reminder service that checks for reminder every 5 sec
- * 
- * Note: this is a public static class, simple call the timer with 
- * TimeService.startReminderService();
+ * currently it has Reminder service that checks for reminder every 5 sec,
+ * it also check if the next day is reached
  *
  */
 import java.text.SimpleDateFormat;
@@ -21,46 +20,75 @@ import javafx.event.EventHandler;
 import javafx.util.Duration;
 
 public final class TimeService {
-	// //for loading of reminder pop ups
-	
-	public static void startReminderService(){
-	Timeline reminderService = new Timeline(new KeyFrame(
-			Duration.seconds(5), new EventHandler<ActionEvent>() {
-				Reminder popUpReminder = Reminder.getInstance();
 
-				@Override
-				public void handle(ActionEvent event) {
-					popUpReminder.startReminder();
-		//			System.out.println("check reminder 5 seconds");
-				}
-			}));
-	reminderService.setCycleCount(Timeline.INDEFINITE);
-	reminderService.play();
+	private static TimeService timeService = new TimeService();
+	SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+	SimpleDateFormat timeFormatter = new SimpleDateFormat("HHmm");
+	String tomorrow = getTomorrowDate();
+	String today = getTodayDate();
+
+	private TimeService() {
 	}
-	
-	public static void startNextDayChecker(){
-		
-		SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
-		
-		// get current system time
-		Calendar todayDate = Calendar.getInstance();
-		Date currentDateTime = todayDate.getTime();
-		String currentDate = dateFormatter.format(currentDateTime);
-		
+
+	public static TimeService getInstance() {
+		return timeService;
+	}
+
+	// //for starting reminder pop ups
+	public void startReminderService() {
 		Timeline reminderService = new Timeline(new KeyFrame(
 				Duration.seconds(5), new EventHandler<ActionEvent>() {
-					
-					//insert event 
-					
+					Reminder popUpReminder = Reminder.getInstance();
+
 					@Override
 					public void handle(ActionEvent event) {
-					//do something insert code here	
-					//System.out.println("check reminder 5 seconds");
+						popUpReminder.startReminder();
+						// System.out.println("check reminder 5 seconds");
 					}
 				}));
 		reminderService.setCycleCount(Timeline.INDEFINITE);
 		reminderService.play();
-		}
 	}
 
+	//check if the next day is reached
+	public void startNextDayChecker() {
 
+		Timeline reminderService = new Timeline(new KeyFrame(
+				Duration.seconds(5), new EventHandler<ActionEvent>() {
+
+					// insert event
+
+					@Override
+					public void handle(ActionEvent event) {
+						// do something insert code here
+						if (today.equals(tomorrow)) {
+							System.out.println("today is tomorrow");
+							today = getTodayDate();
+							tomorrow = getTomorrowDate();
+						} else {
+							today = getTodayDate();
+							System.out.println("today is not tomorrow");
+						}
+					}
+				}));
+		reminderService.setCycleCount(Timeline.INDEFINITE);
+		reminderService.play();
+	}
+
+	private String getTomorrowDate() {
+		Date date = new Date();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.add(Calendar.DATE, 1);
+		date = calendar.getTime();
+		return dateFormatter.format(date);
+	}
+
+	private String getTodayDate() {
+		Date today = new Date();
+		Calendar calendar = Calendar.getInstance();
+		today = calendar.getTime();
+		return dateFormatter.format(today);
+	}
+
+}
